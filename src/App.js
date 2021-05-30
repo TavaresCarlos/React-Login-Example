af8621778c67235822fbc023fa4ddfaedffa7c59
Carlos Henrique Tavares <tavares.carlos.h@gmail.com>
2021-05-30 19:37:27 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import Content from './components/content';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Content = () => (
+	<div>
+		<h1>Content</h1>
+		<form>
+			<button>Clique aqui</button>
+		</form>
+	</div>
+);
+
+export default Content;
